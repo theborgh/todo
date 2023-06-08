d4fc74ae3ce5b47e86a53ea41c4ddae4c0d5be65
theborgh <borghiborghi@gmail.com>
2023-06-08 21:09:26 +0200

    implement basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./style.css";
+import { Todo } from "./Todo";
 
-function App() {
-  const [count, setCount] = useState(0)
+type TodoItem = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+const initialTodos: TodoItem[] = [
+  { id: 1, text: "Todo 1", completed: false },
+  { id: 2, text: "Todo 2", completed: true },
+  { id: 3, text: "Todo 3", completed: false },
+];
+
+const App = () => {
+  const [todos, setTodos] = useState<TodoItem[]>(initialTodos);
+
+  const addTodo = (text: string) => {
+    const newTodo: TodoItem = {
+      id: Math.max(...todos.map((todo) => todo.id)) + 1,
+      text: text,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleCompleted = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Todo List</h1>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            toggleCompleted={toggleCompleted}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
+      <div className="form-container">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const input = e.target.elements.todo as HTMLInputElement;
+            if (input.value.trim() !== "") {
+              addTodo(input.value.trim());
+            }
+            input.value = "";
+          }}
+        >
+          <input
+            type="text"
+            name="todo"
+            className="form__input"
+            placeholder="Add new todo"
+          />
+          <button className="form__button" type="submit">
+            Add
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Todo.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type Props = {
+  todo: {
+    id: number;
+    text: string;
+    completed: boolean;
+  };
+  toggleCompleted: (id: number) => void;
+  deleteTodo: (id: number) => void;
+};
+
+export const Todo = (props: Props) => {
+  const { todo, toggleCompleted, deleteTodo } = props;
+
+  return (
+    <li className="todo-item">
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleCompleted(todo.id)}
+      />
+      <span
+        className={todo.completed ? "todo-text completed" : "todo-text"}
+        onClick={() => toggleCompleted(todo.id)}
+      >
+        {todo.text}
+      </span>
+      <button className="todo-delete-btn" onClick={() => deleteTodo(todo.id)}>
+        X
+      </button>
+    </li>
+  );
+};
